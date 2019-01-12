706717239cc011744a804547129a7607f38733a5
dipsdeepak11 <deepak.yadav@xoriant.com>
2019-01-13 01:50:22 +0530

    validators

---
+++ tweetme/tweets/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Tweet
+from django.core.exceptions import ValidationError
+
+class TweetModelForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        Fields = [
+            'user',
+            'content'
+        ]
+
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data.get('content')
+        if content == 'abc':
+            raise ValidationError('its cant be abc(from forms)')
+        return content
